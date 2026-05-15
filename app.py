4fb5a36a6ceb8ca241dba23aec3cce1f1714482e
avenue17 <daniel080326@gmail.com>
2026-05-15 15:51:38 +0900

    fix app.py syntax

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ def main():
     app = Container().build_app()
     port = int(os.environ.get('PORT', 7860))
     app.launch(
-        server_name="0.0.0.0"
+        server_name="0.0.0.0",
         server_port=port
     )
 
